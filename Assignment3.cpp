b7e856e35dcc80078b4b1f67bc6ed2117da18f78
Clark Smith <clark.a.smith-1@ou.edu>
2023-02-21 19:35:28 -0600

    added ProductArray method

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -12,3 +12,14 @@ int SumArray(vector<int> vec) {
 
 	return sum;
 }
+
+int ProductArray(vector<int> vec) {
+
+	int product = 1;
+
+	for (int i = 0; i < vec.size(); i++) {
+		product *= vec.at(i);
+	}
+
+	return product;
+}
